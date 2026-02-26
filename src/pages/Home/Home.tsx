b4d53625f8bc390ee16dfb3cf661960b92634755
taehyung5271 <kth14200@gmail.com>
2026-02-26 09:41:03 +0900

    대소문자 정리5

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,24 @@
-import About from "./Sections/About";
-import Archiving from "./Sections/Archiving";
-import Skills from "./Sections/Skills";
-import Projects from "./Sections/Projects";
-import Career from "./Sections/Career";
-import Header from "../../Widgets/Header/Header";
-import Footer from "../../Widgets/Footer/Footer";
-import styles from "./Home.module.css"
+import About from "./sections/About";
+import Archiving from "./sections/Archiving";
+import Skills from "./sections/Skills";
+import Projects from "./sections/Projects";
+import Career from "./sections/Career";
+import Header from "../../widgets/Header/Header";
+import Footer from "../../widgets/Footer/Footer";
+import styles from "./Home.module.css";
 
 const Home = () => {
-
-    return (
-        <div className={styles.container}>
-            <Header/>
-            <About/>
-            <Skills/>
-            <Archiving/>
-            <Projects/>
-            <Career/>
-            <Footer/>
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <Header />
+      <About />
+      <Skills />
+      <Archiving />
+      <Projects />
+      <Career />
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
